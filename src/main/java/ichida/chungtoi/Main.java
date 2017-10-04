2757565ce4cbb50001f398be3df5b5e7bfc67cbb
aichida <alexandre.ichida@neogrid.com>
2017-10-04 11:35:32 -0300

    nova interface de jogo

---
+++ src/main/java/ichida/chungtoi/Main.java
@@ -0,0 +1,101 @@
+package ichida.chungtoi;
+
+import ichida.chungtoi.integration.ChungToiInterface;
+import ichida.chungtoi.model.Player;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.Scanner;
+
+import static ichida.chungtoi.util.ClientConstants.*;
+import static ichida.chungtoi.util.ResultConstants.*;
+
+public class Main {
+
+    private static ChungToiInterface client;
+
+    public static void main(String... args) throws Exception {
+        System.out.println("Defina seu nome:");
+        Scanner input = new Scanner(System.in);
+        String playerName = input.next();
+        int option = -1;
+        System.out.println("Conectando com o servidor...");
+        try {
+            client = connect("localhost");
+        } catch (Exception e) {
+            System.out.println("Erro ao conectar com o servidor do jogo.");
+            e.printStackTrace();
+            System.exit(0);
+            return;
+        }
+        System.out.println("Conexão estabelecida");
+
+        Player player = registerPlayer(playerName);
+        if (player != null) {
+            System.out.println("Jogador Registrado: " + player);
+
+
+        }
+    }
+
+    public static ChungToiInterface connect(String serverHost) throws Exception {
+        return (ChungToiInterface) Naming.lookup("//" + serverHost + "/chungtoi");
+    }
+
+    public static Player registerPlayer(String name) throws Exception {
+        Player player = new Player(name);
+        int result = client.createPlayer(name);
+        if (result == PLAYER_ALREADY_REGISTERED) {
+            System.out.println("Jogador " + name + " já cadastrado");
+            return null;
+        } else if (result == PLAYER_LIMIT_REACHED) {
+            System.out.println("Limite de jogadores atingido");
+            return null;
+        } else {
+            player.setId(result);
+            return player;
+        }
+    }
+
+    public static void showMenu(Player player) throws Exception {
+        int hasGameResult = client.hasGame(player.getId());
+        while (hasGameResult == NO_GAME) {
+            Thread.sleep(1000L);
+            hasGameResult = client.hasGame(player.getId());
+        }
+        if (hasGameResult == REGISTER_TIMEOUT) {
+            System.out.println("Tempo limite de espera atingido");
+        } else if (hasGameResult == OK) {
+            String opponentName = client.getOppositePlayer(player.getId());
+            System.out.println("Seu adversário: " + opponentName);
+        } else {
+            // Inconsistência do servidor
+            System.out.println("Retorno do método hasGame inesperado: " + hasGameResult);
+        }
+    }
+
+    public static void showGameMenu(Player player, String opponent) throws Exception {
+        boolean gameRunning = true;
+        while (gameRunning) {
+            int gameStatus = client.isMyTurn(player.getId());
+            System.out.println("Esperando o turno do adversário");
+            while (gameStatus == ADVERSARY_TURN) {
+                Thread.sleep(1000L);
+                gameStatus = client.isMyTurn(player.getId());
+            }
+            if (gameStatus == WINNER) {
+                gameRunning = false;
+                System.out.println("Você venceu");
+            } else if (gameStatus == LOSER) {
+                System.out.println("Voce perdeu");
+            } else if (gameStatus == PLAYER_TURN) {
+                // Insere se não tiver tres peças
+                // se ja tiver tres peças, move
+                // a contagem de peças pode ser controlada no lado do cliente
+            }
+        }
+
+    }
+
+
+}
